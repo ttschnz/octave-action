b327b4945a977a69c9435b2feffa7cf8828f5b60
Tim T. <tim@mw3.ch>
2024-10-26 11:31:48 +0200

    extend workflow to test string_to_check works

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,7 @@ echo -e "Octave output:\n$output"
 string_to_check="$2"
 # Check if the string was found
 if echo "$output" | grep -q "$string_to_check"; then
+    echo "output matched '$string_to_check'"
     exit 1
 fi
+echo "output didn't match '$string_to_check'"
